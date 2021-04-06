abd1aece0790cd18b6268daf8baa2ed6f09c58cd
lucasoliveiraa <lucasjackcosta18@gmail.com>
2021-04-06 00:06:50 -0300

    :sparkles: Componentes e páginas

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>Oláaa</h1>
+    </>
+  )
+}
