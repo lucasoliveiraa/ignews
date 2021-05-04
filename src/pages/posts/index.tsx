26ff145c7efe2a3aaa09555437a1dcfb3b0be06c
lucasoliveiraa <lucasjackcosta18@gmail.com>
2021-05-03 23:27:54 -0300

    chore: trabalhando com CMS

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,72 @@
+import Head from "next/head";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import styles from './styles.module.scss';
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface ProtsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: ProtsProps) {
+  return(
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['posts.title', 'posts.content'],
+    pageSize: 100,
+  })
+
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
+
+  // console log em cascata ou conteudos internos(ARRAYS ou OBJECT)
+  // console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
